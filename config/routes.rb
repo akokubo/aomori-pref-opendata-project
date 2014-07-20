03fd322e86467d930efff184f07346e71277c82f
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-07-20 10:00:34 +0900

    Homeコントローラー、Category、Place、PlaceImageのScaffoldの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  resources :place_images
+
+  resources :places
+
+  resources :categories
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
